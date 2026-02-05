f2d85f833367663930715542dd8a80d3c70dd748
Nischal Shrestha <sthanischal5060@gmail.com>
2026-02-05 13:27:31 +0545

    practice question solution

---
+++ practice1.py
@@ -0,0 +1,22 @@
+class Student:
+    # def __init__(self,name,mark1,mark2,mark3):
+    #     self.name=name
+    #     self.mark1=mark1
+    #     self.mark2=mark2
+    #     self.mark3=mark3
+    def __init__(self,name,*mark):
+        self.name=name
+        self.total=sum(mark)
+        
+    def average_marks(self):
+        average=self.total/3
+        average=round(average,2)
+        return average
+
+s1=Student("Nischal",96,91,90)
+print(s1.name)
+print(s1.average_marks())
+
+s1.name="Nirjal"
+print(s1.name)
+print(s1.average_marks())
