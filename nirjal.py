966db7b5b69ef3ab4b34598d20a8f3dd42518469
Nischal Shrestha <sthanischal5060@gmail.com>
2026-03-07 21:14:30 +0545

    teaching my brother

---
+++ nirjal.py
@@ -0,0 +1,54 @@
+
+# # to find odd and even
+# n =int(input("enter number:"))
+# if n%2==0:
+#     print('even')
+# else:
+#     print('odd')
+
+
+
+
+# postive ,negative and zero
+
+# a=int(input('enter number: '))
+# if a>0:
+#     print("Positive")
+# elif a<0:
+#     print("negative")
+# else:
+#     print("zero")
+
+
+
+# n=int(input("enter first number:"))
+# s=int(input("enter second number:"))
+# if n>s:
+#     print("larger is first number")
+#     print(n)
+# else:
+#     print("larger is second number")
+#     print(s)
+
+
+
+
+#for i in range (0,18,2):
+   # print(i)
+   
+
+# for i in range (1,20,2):
+#     print (i)
+
+
+# nrg=input("enter nummber:")
+# print(nrg)
+      
+   
+
+
+
+
+
+
+
