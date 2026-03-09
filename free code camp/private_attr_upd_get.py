8044b1e5ebdca510a42d8e58841ff72df23d66e5
Nischal Shrestha <sthanischal5060@gmail.com>
2026-03-09 20:52:31 +0545

    accessing private and protected variable

--- free code camp/private_attr_upd_get.py
+++ free code camp/private_attr_upd_get.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class User:
     def __init__(self,username,password,email):
         self.username=username
@@ -7,6 +8,23 @@ class User:
         self.__password=password
         return self.__password
     
+    def get_email(self):
+        return self._email
+
+    def set_email(self,new_email):
+        try:
+            if "@" not in new_email:
+                raise Exception("invalid email")
+            else:self._email=new_email
+            
+        except Exception as e:
+            print(f"{e}")
+        
 user1=User("Nischal","123","nischal@gmail.com")
 # print(user1._email)
-print(user1.change_password("977"))
+# print(user1.change_password("977"))
+# print(user1.get_email())
+user1.set_email("hello@gmail.com")
+print(f"Email is updatet at {datetime.now()}")
+print(user1.get_email())
+
