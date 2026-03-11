10dc2379965c5dbd7d7bc88bc23d804f0ae4c5dd
Nischal Shrestha <sthanischal5060@gmail.com>
2026-03-11 19:56:03 +0545

    private and protected

--- free code camp/static_vs_instance.py
+++ free code camp/static_vs_instance.py
@@ -6,12 +6,19 @@ class BankAccount:
         self._amount=amount
 
     def deposite(self,amount):
-        if amount>0:
+        if self._is_valid_amount(amount):
             self._amount+=amount
+            self.__log_transaction("deposite",amount)
             print(f"{self.owner}'s new balance : ${self._amount}")
+            
         else:
             print("Amount must be greater then 0")
+    def _is_valid_amount(self,amount):
+        return amount>0
     
+    def __log_transaction(self,transcation_type,ammount):
+        print(f"Logging {transcation_type} of ${ammount}")
+       
     @staticmethod
     def is_valid_interest_rate(rate):
         return rate>=0 and rate<=5
@@ -19,9 +26,9 @@ class BankAccount:
             
 account1=BankAccount("Nischal",1000)
 account2=BankAccount("Norman",300)
-account1.deposite=200
-account2.deposite=4000
+account1.deposite(400)
+# account2.deposite=4000
 
-print(BankAccount.is_valid_interest_rate(2))
-print(account1.is_valid_interest_rate(6))
+# print(BankAccount.is_valid_interest_rate(2))
+# print(account1.is_valid_interest_rate(6))
         
