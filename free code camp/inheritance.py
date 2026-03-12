e2731d288e29b01b5a681138575b5db2ebc5ca90
Nischal Shrestha <sthanischal5060@gmail.com>
2026-03-12 21:53:55 +0545

    interitance

---
+++ free code camp/inheritance.py
@@ -0,0 +1,28 @@
+# inherantance
+
+class Vehicle:
+    def __init__(self,brand,model,year):
+        self.brand=brand
+        self.model=model
+        self.year=year
+        
+    def start(self):
+        print("Vehicle is starting")
+    def stop(self):
+        print("Vehicle is stopping")
+
+class Car(Vehicle):
+    
+    def __init__(self,brand,model,year,number_of_doors,number_of_wheels):
+        super().__init__(brand,model,year)
+        self.number_of_doors=number_of_doors
+        self.number_of_wheels=number_of_wheels
+
+class Bike(Vehicle):
+    def __init__(self,brand,model,year,number_of_wheels):
+        super().__init__(brand,model,year)
+        self.number_of_wheels=number_of_wheels
+
+car1=Car("Toyota","Mustang",2010,4,4)
+car1.stop()
+print(car1.__dict__)
