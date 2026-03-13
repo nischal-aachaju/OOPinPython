e24afd1c901f0e8603497ed7f33d3c466b27ac93
Nischal Shrestha <sthanischal5060@gmail.com>
2026-03-13 19:46:12 +0545

    polymorphism 2.0

--- free code camp/polymorphism.py
+++ free code camp/polymorphism.py
@@ -1,29 +1,69 @@
 # polymorphism
-class Car():
+
+
+# if we dont apply polymorphism then---------------
+# class Car():
+    
+#     def __init__(self,brand,model,year,number_of_doors,number_of_wheels):
+#         self.brand=brand
+#         self.model=model
+#         self.year=year
+#         self.number_of_doors=number_of_doors
+#         self.number_of_wheels=number_of_wheels
+        
+#     def start_car(self):
+#         print("car is starting")
+#     def stop_car(self):
+#         print("car is stopping")
+
+# class Bike():
+#     def __init__(self,brand,model,year,number_of_wheels):
+#         self.brand=brand
+#         self.model=model
+#         self.year=year
+#         self.number_of_wheels=number_of_wheels
+
+#     def start_bike(self):
+#         print("bike is starting")
+#     def stop_bike(self):
+#         print("bike is stopping")
+
+# for vehicle in vehicles:
+#     if isinstance(vehicle,Car):
+#         print(f"Inspecting {vehicle.brand} {vehicle.model} of model {vehicle.year} {type(vehicle).__name__}")
+#         vehicle.start()
+#         vehicle.stop()
+#     elif isinstance(vehicle,Bike):
+#         print(f"Inspecting {vehicle.brand} {vehicle.model} of model {vehicle.year} {type(vehicle).__name__}")
+#         vehicle.start()
+#         vehicle.stop()
+
+# -----------------------------------------------------
+
+# if we apply polymorphism and inheritance then, we can make same program for more tham 2 Classes
+class Vehicle:
+    def __init__(self,brand,model,year):
+        self.brand=brand
+        self.model=model
+        self.year=year
+        
+    def start(self):
+        print("Vehicle is starting")
+    def stop(self):
+        print("Vehicle is stopping")
+
+class Car(Vehicle):
     
     def __init__(self,brand,model,year,number_of_doors,number_of_wheels):
-        self.brand=brand
-        self.model=model
-        self.year=year
+        super().__init__(brand,model,year)
         self.number_of_doors=number_of_doors
         self.number_of_wheels=number_of_wheels
-        
-    def start_car(self):
-        print("car is starting")
-    def stop_car(self):
-        print("car is stopping")
 
-class Bike():
+class Bike(Vehicle):
     def __init__(self,brand,model,year,number_of_wheels):
-        self.brand=brand
-        self.model=model
-        self.year=year
+        super().__init__(brand,model,year)
         self.number_of_wheels=number_of_wheels
 
-    def start_bike(self):
-        print("bike is starting")
-    def stop_bike(self):
-        print("bike is stopping")
 
 # list of vehicle to inspect
 vehicles=[
@@ -32,12 +72,10 @@ vehicles=[
 
 ]
 
+
 for vehicle in vehicles:
-    if isinstance(vehicle,Car):
-        print(f"Inspecting {vehicle.brand} {vehicle.model} of model {vehicle.year} {type(vehicle).__name__}")
-        vehicle.start_car()
-        vehicle.stop_car()
-    elif isinstance(vehicle,Bike):
-        print(f"Inspecting {vehicle.brand} {vehicle.model} of model {vehicle.year} {type(vehicle).__name__}")
-        vehicle.start_bike()
-        vehicle.stop_bike()
+    if isinstance(vehicle,Vehicle):
+         print(f"Inspecting {vehicle.brand} {vehicle.model} of model {vehicle.year} {type(vehicle).__name__}")
+         vehicle.start()
+         vehicle.stop()
+
