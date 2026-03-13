7cf35f8e848178b08523cac88afc67ddfad6e9d0
Nischal Shrestha <sthanischal5060@gmail.com>
2026-03-13 19:27:52 +0545

    polymorphism

--- free code camp/polymorphism.py
+++ free code camp/polymorphism.py
@@ -1,32 +1,43 @@
-# inherantance
-
-class Vehicle:
-    def __init__(self,brand,model,year):
+# polymorphism
+class Car():
+    
+    def __init__(self,brand,model,year,number_of_doors,number_of_wheels):
         self.brand=brand
         self.model=model
         self.year=year
-        
-
-
-class Car(Vehicle):
-    
-    def __init__(self,brand,model,year,number_of_doors,number_of_wheels):
-        super().__init__(brand,model,year)
         self.number_of_doors=number_of_doors
         self.number_of_wheels=number_of_wheels
         
     def start_car(self):
-        print("Vehicle is starting")
+        print("car is starting")
     def stop_car(self):
-        print("Vehicle is stopping")
+        print("car is stopping")
 
-class Bike(Vehicle):
+class Bike():
     def __init__(self,brand,model,year,number_of_wheels):
-        super().__init__(brand,model,year)
+        self.brand=brand
+        self.model=model
+        self.year=year
         self.number_of_wheels=number_of_wheels
 
     def start_bike(self):
-        print("Vehicle is starting")
+        print("bike is starting")
     def stop_bike(self):
-        print("Vehicle is stopping")
+        print("bike is stopping")
 
+# list of vehicle to inspect
+vehicles=[
+    Car("Ford","Mustang",2009,4,4),
+    Bike("BMW","S100RR",2022,2)
+
+]
+
+for vehicle in vehicles:
+    if isinstance(vehicle,Car):
+        print(f"Inspecting {vehicle.brand} {vehicle.model} of model {vehicle.year} {type(vehicle).__name__}")
+        vehicle.start_car()
+        vehicle.stop_car()
+    elif isinstance(vehicle,Bike):
+        print(f"Inspecting {vehicle.brand} {vehicle.model} of model {vehicle.year} {type(vehicle).__name__}")
+        vehicle.start_bike()
+        vehicle.stop_bike()
