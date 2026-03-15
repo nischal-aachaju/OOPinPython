a6f6525143a5e504a94c26862d11b20824a30bd1
Nischal Shrestha <sthanischal5060@gmail.com>
2026-03-15 10:34:58 +0545

    borrow book feature done

--- practices/library_management_system.py
+++ practices/library_management_system.py
@@ -16,15 +16,39 @@ class Book:
 
     def is_available(self):
         if not self.__is_borrowed:
-            return "available"
+            return True
         
         else:
-            return "not available"
+            return False
     
     def __str__(self):
+        self.is_book_available="available" if self.is_available() else "not available"
+        return f"{self.title} by {self.author} which is {self.is_book_available} "
+
+class Member:
+    
+    def __init__(self,name,member_id):
+        self.name=name
+        self.member_id=member_id
+        self.__borrowed_books=[]
+
+    def borrow_book(self,book):
         
-        return f"{self.title} by {self.author} which is {self.is_available()} "
-b1=Book("Munamodhan","laxmi persad devkota",1232)
-b1.borrow()
-print(b1.is_available())
-print(b1)
+        if book.is_available():
+            self.__borrowed_books.append(book)
+            book.borrow()
+            print(f"{book.author} is successfully Booked by {self.name}")
+        else:
+            print(f"{book.author} is already Booked")
+    
+    
+    def __str__(self):
+        return f"{self.name} borrowed {[book.title for book in self.__borrowed_books]}"
+
+# b1=Book("Muna Madan", "Laxmi parshad Devokta",121)
+# print(b1)
+# m1=Member("Nischal",121)
+# print(m1)
+# m1.borrow_book(b1)
+# print(m1)
+# print(b1)
